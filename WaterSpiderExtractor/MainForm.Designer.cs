6b0be48317cc9b26d5db113a66b3ab8dbc1a2209
Wold0110 <walter20020110@gmail.com>
2022-07-08 11:29:46 +0200

    Add project files.

---
+++ WaterSpiderExtractor/MainForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WaterSpiderExtractor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.connStatusPanel = new System.Windows.Forms.Panel();
+            this.fromDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.toDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.downloadBtn = new System.Windows.Forms.Button();
+            this.timelimitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // connStatusPanel
+            // 
+            this.connStatusPanel.Location = new System.Drawing.Point(131, 12);
+            this.connStatusPanel.Name = "connStatusPanel";
+            this.connStatusPanel.Size = new System.Drawing.Size(18, 15);
+            this.connStatusPanel.TabIndex = 0;
+            // 
+            // fromDatePicker
+            // 
+            this.fromDatePicker.Location = new System.Drawing.Point(12, 56);
+            this.fromDatePicker.Name = "fromDatePicker";
+            this.fromDatePicker.Size = new System.Drawing.Size(200, 23);
+            this.fromDatePicker.TabIndex = 1;
+            // 
+            // toDatePicker
+            // 
+            this.toDatePicker.Location = new System.Drawing.Point(12, 105);
+            this.toDatePicker.Name = "toDatePicker";
+            this.toDatePicker.Size = new System.Drawing.Size(200, 23);
+            this.toDatePicker.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tól";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ig";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Csatlakozás Állapota";
+            // 
+            // downloadBtn
+            // 
+            this.downloadBtn.Location = new System.Drawing.Point(12, 146);
+            this.downloadBtn.Name = "downloadBtn";
+            this.downloadBtn.Size = new System.Drawing.Size(200, 23);
+            this.downloadBtn.TabIndex = 7;
+            this.downloadBtn.Text = "Letöltés";
+            this.downloadBtn.UseVisualStyleBackColor = true;
+            this.downloadBtn.Click += new System.EventHandler(this.downloadBtn_Click);
+            // 
+            // timelimitBtn
+            // 
+            this.timelimitBtn.Location = new System.Drawing.Point(12, 175);
+            this.timelimitBtn.Name = "timelimitBtn";
+            this.timelimitBtn.Size = new System.Drawing.Size(200, 26);
+            this.timelimitBtn.TabIndex = 8;
+            this.timelimitBtn.Text = "Időben/Túlment";
+            this.timelimitBtn.UseVisualStyleBackColor = true;
+            this.timelimitBtn.Click += new System.EventHandler(this.timelimitBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 227);
+            this.Controls.Add(this.timelimitBtn);
+            this.Controls.Add(this.downloadBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toDatePicker);
+            this.Controls.Add(this.fromDatePicker);
+            this.Controls.Add(this.connStatusPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Letöltő";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Panel connStatusPanel;
+        private DateTimePicker fromDatePicker;
+        private DateTimePicker toDatePicker;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button downloadBtn;
+        private Button timelimitBtn;
+    }
+}
---
+++ WaterSpiderExtractor/MainForm.cs
@@ -0,0 +1,170 @@
+using MySql.Data.MySqlClient;
+
+namespace WaterSpiderExtractor
+{
+    public partial class MainForm : Form
+    {
+        const string DATABASE_FILE = "database.cfg";
+        MySqlConnection waterspider;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            
+            toDatePicker.MaxDate = DateTime.Today;
+            fromDatePicker.MaxDate = DateTime.Today;
+            DateTime firsttime = DateTime.Parse("2022-05-10");
+            toDatePicker.MinDate = firsttime;
+            fromDatePicker.MinDate = firsttime;
+
+
+            Init();
+            if(waterspider.State == System.Data.ConnectionState.Open)
+                connStatusPanel.BackColor = Color.Green;
+            else
+                connStatusPanel.BackColor = Color.Red;
+
+        }
+        void Init()
+        {
+            string host="", database="", user = "", passwd = "";
+            foreach (string line in File.ReadAllLines(DATABASE_FILE))
+            {
+                try
+                {
+                    string[] parts = line.Split('=');
+                    switch (parts[0])
+                    {
+                        case "host":
+                            host = parts[1];
+                            break;
+                        case "database":
+                            database = parts[1];
+                            break;
+                        case "user":
+                            user = parts[1];
+                            break;
+                        case "passwd":
+                            passwd = parts[1];
+                            break;
+                        default:
+                            MessageBox.Show("Hibás sor: " + line);
+                            break;
+                    }
+                }
+                catch {}
+            }
+            string connectString = $"server={host};database={database};userid={user};password={passwd}";
+            waterspider = new MySqlConnection(connectString);
+            try { waterspider.Open(); }
+            catch { }
+        }
+        List<WaterspiderRecord> GetData(string from, string to)
+        {
+            List<WaterspiderRecord> result = new List<WaterspiderRecord>();
+            //SELECT * FROM `train_records` WHERE `tol` >= '2022-06-01' AND `ig` <= '2022-06-30' ORDER BY `tol`
+            string select = $"SELECT * FROM `train_records` WHERE `tol` >= '{from}' AND `ig` <= '{to}' ORDER BY `tol`";
+            MySqlCommand selectCMD = new MySqlCommand(select, waterspider);
+            MySqlDataReader rdr = selectCMD.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                result.Add(new(rdr.GetString(0),rdr.GetString(1),rdr.GetString(2),rdr.GetString(3),rdr.GetString(4)));
+            }
+            rdr.Close();
+            return result;
+        }
+        bool GetPath(string defaultName,out string filename)
+        {
+            SaveFileDialog sfd = new();
+            sfd.Title = "Mentés helye";
+            sfd.DefaultExt = "csv";
+            sfd.Filter = "CSV Fájlok (*.csv)|*.csv|Minden fájl (*.*)|*.*";
+            sfd.FileName = defaultName;
+            sfd.FilterIndex = 1;
+            sfd.RestoreDirectory = true;
+            filename = "";
+            if (DialogResult.OK == sfd.ShowDialog())
+            {
+                filename = Path.GetFullPath(sfd.FileName);
+                return true;   
+            }
+            return false;
+        }
+        void GetDates(out string from, out string to)
+        {
+            from = fromDatePicker.Value.ToString("yyyy-MM-dd");
+            to = toDatePicker.Value.ToString("yyyy-MM-dd");
+        }
+        private void downloadBtn_Click(object sender, EventArgs e)
+        { 
+            string path;
+            if(GetPath("nyers_adat.csv", out path))
+            {
+                string from, to;
+                GetDates(out from, out to);
+                List<WaterspiderRecord> wsr = GetData(from, to);
+                File.WriteAllText(path, "Vonat,Vezeto,Hiba,Tol,Ig\r\n");
+                wsr.ForEach(x => File.AppendAllText(path, x.CSV));
+                MessageBox.Show("Kész");
+            }
+        }
+
+        private void timelimitBtn_Click(object sender, EventArgs e)
+        {
+            string path;
+            if (GetPath("napi_adat.csv", out path))
+            {
+                try
+                {
+                    string from, to;
+                    GetDates(out from, out to);
+                    List<WaterspiderRecord> wsr = GetData(from, to);
+                    List<Dailydata> ddl = new List<Dailydata>();
+                    for (DateTime i = DateTime.Parse(from); i <= DateTime.Parse(to); i = i.AddDays(1))
+                    {
+                        Dailydata dd = new(i);
+                        while (wsr.Count > 0 && wsr[0].Day(i))
+                        {
+                            if (wsr[0].InTime) dd.inTime++;
+                            else dd.overTime++;
+                            wsr.RemoveAt(0);
+                        }
+                        ddl.Add(dd);
+                    }
+                    File.WriteAllText(path, "Nap,Idoben,Tulment\r\n");
+                    ddl.ForEach(x => { if (!(x.inTime == x.overTime && x.overTime == 0)) { File.AppendAllText(path, x.CSV); } });
+
+                    MessageBox.Show("Kész");
+                }
+                catch(IOException ex) { MessageBox.Show("A fájl nyitva van. Kérlek zárd be."); }
+            }
+        }
+    }
+    struct Dailydata{
+        public DateTime date;
+        public int inTime = 0;
+        public int overTime = 0;
+        public Dailydata(DateTime date) => this.date = date;
+        public string CSV => date + "," + inTime + "," + overTime + "\r\n";
+    }
+    class WaterspiderRecord
+    {
+        internal string vonat;
+        internal string vezeto;
+        internal string hiba;
+        internal string start;
+        internal string stop;
+        public WaterspiderRecord(string vonat,string vezeto, string hiba,string start,string stop)
+        {
+            this.vonat = vonat;
+            this.vezeto = vezeto;
+            this.hiba = hiba;
+            this.start = start;
+            this.stop = stop;
+        }
+        public string CSV => vonat + "," + vezeto + "," + hiba + "," + start + "," + stop+"\r\n";
+        public bool InTime => DateTime.Parse(stop).Subtract(DateTime.Parse(start)).TotalMinutes <= 120;
+        public bool Day(DateTime day) => day.Date == DateTime.Parse(start).Date;
+    }
+}
